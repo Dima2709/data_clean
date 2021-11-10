41e29b5376a8b356919dcf45c2e30108fada3325
Dima2709 <87536237+Dima2709@users.noreply.github.com>
2021-11-10 21:30:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ log = pd.read_csv('log.csv', header = None)
 
 log.columns = ['user_id', 'time', 'bet', 'win']
 
+
+#4) оставим в поле user_id значение типа: "user_N", где N значение идентификатора;
+
+mass2 = []
+for i in log['user_id']:
+    mass2.append(i.split(' ')[-1])
+for i in range(len(mass2)):
+    log['user_id'][i] = mass2[i]
+
 #5) уберём начальную скобку из поля time.
 
 mass = []
@@ -23,16 +32,11 @@ for i in mass:
 for i in range(len(mass1)):
     log['time'][i] = mass1[i]
 
-#4) оставим в поле user_id значение типа: "user_N", где N значение идентификатора;
-
-mass2 = []
-for i in log['user_id']:
-    mass2.append(i.split(' ')[-1])
-for i in range(len(mass2)):
-    log['user_id'][i] = mass2[i]
 
 #3) удалим строки, которые содержат значения user_id с ошибками;
 
 for i in range(len(log['user_id'])):
     if log['user_id'][i] == '#error':
         log.drop(labels = [i], axis = 0,inplace=True)
+
+print(log)
